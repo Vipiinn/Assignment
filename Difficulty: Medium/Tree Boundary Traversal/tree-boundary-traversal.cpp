bf086875dee786fbeda9f8aeaba218e6902907b5
Vipin Vishwakarma <vipinkumarvishwakarma837@gmail.com>
2026-02-06 00:59:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.cpp
+++ Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.cpp
@@ -1,53 +1,54 @@
+
+
 class Solution {
   public:
   
-    void leftBoundary(Node *root , vector<int>& ans){
+    void leftBoundary(Node* root , vector<int>& ans){
         if(root == NULL) return;
         if(root->left == NULL && root->right == NULL) return;
         
         ans.push_back(root->data);
         
-        if(root->left)
-            leftBoundary(root->left , ans);
-        else
-            leftBoundary(root->right , ans);
+        if(root->left) leftBoundary(root->left,ans);
+        else leftBoundary(root->right,ans);
     }
     
-    void bottomBoundary(Node* root , vector<int>& ans){
+    void leafNode(Node* root , vector<int>& ans){
         if(root == NULL) return;
         
         if(root->left == NULL && root->right == NULL){
             ans.push_back(root->data);
             return;
         }
+            
+        leafNode(root->left,ans);
+        leafNode(root->right,ans);
         
-        bottomBoundary(root->left , ans);
-        bottomBoundary(root->right , ans);
     }
+
     
-    void rightBoundary(Node* root , vector<int>&ans){
+    void rightBoundary(Node* root , vector<int>& ans){
         if(root == NULL) return;
         if(root->left == NULL && root->right == NULL) return;
         
-        if(root->right)
-            rightBoundary(root->right , ans);
-        else
-            rightBoundary(root->left , ans);
-            
+        if(root->right) rightBoundary(root->right,ans);
+        else rightBoundary(root->left , ans);
         ans.push_back(root->data);
+        
     }
   
     vector<int> boundaryTraversal(Node *root) {
-        vector<int> ans;
+        vector<int>ans;
         if(root == NULL) return ans;
+        ans.push_back(root->data);
         
-        ans.push_back(root->data);          // root
+        leftBoundary(root->left,ans);
+        leafNode(root->left,ans);
+        leafNode(root->right , ans);
+        rightBoundary(root->right,ans);
         
-        leftBoundary(root->left, ans);      // left boundary
-        bottomBoundary(root->left, ans);    // leaves
-        bottomBoundary(root->right, ans);
-        rightBoundary(root->right, ans);    // right boundary
         
         return ans;
+        
     }
 };
