478fc48f4318d7a85ea96494a7538a08f0891469
Vipin Vishwakarma <vipinkumarvishwakarma837@gmail.com>
2026-02-07 23:58:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.cpp
+++ Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.cpp
@@ -4,42 +4,36 @@ class Solution {
     vector<int> bottomView(Node *root) {
         
         vector<int>ans;
+        
         if(root == NULL) return ans;
+         
+        map<int,int>mp; // type(hd,root->data)
+        queue<pair<Node*,int>>q; //type(Node* , hd)
         
-        map<int,map<int,vector<int>>>mp; // (hd,(level,list))
-        queue<pair<Node* ,pair<int,int>>>q; // (Node,(hd,level))
-        
-        q.push(make_pair(root,make_pair(0,0)));
+        q.push(make_pair(root , 0));
         
         while(q.size() > 0){
-            pair<Node* ,pair<int,int>> temp = q.front();
+            
+            pair<Node*,int> temp = q.front();
             q.pop();
             
             Node* frontNode = temp.first;
-            int hd = temp.second.first;
-            int lvl = temp.second.second;
+            int hd = temp.second;
             
-            mp[hd][lvl].push_back(frontNode->data);
+            mp[hd] = frontNode->data;
             
             if(frontNode->left){
-                q.push(make_pair(frontNode->left , make_pair(hd-1 , lvl+1)));
+                q.push(make_pair(frontNode->left , hd-1));
             }
-            
             if(frontNode->right){
-                q.push(make_pair(frontNode->right , make_pair(hd+1 , lvl+1)));
+                q.push(make_pair(frontNode->right , hd+1));
             }
         }
         
         for(auto i : mp){
-            vector<int>temp;
-            for(auto j : i.second){
-                for(auto k : j.second){
-                    temp.push_back(k);
-                }
-            }
-            ans.push_back(temp[temp.size()-1]);
+            ans.push_back(i.second);
         }
-        return ans;
         
+        return ans;
     }
 };
