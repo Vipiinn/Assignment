763ab8fe4761eeb2c82a0ff50d775a35ad2587a4
Vipin Vishwakarma <vipinkumarvishwakarma837@gmail.com>
2026-03-23 01:22:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,36 +1,33 @@
 class Solution {
   public:
   
-   int dp[1003][1003];
-  
+    int dp[1002][1002];
+    
     int knap(vector<int>&val , vector<int>&wt , int W , int n){
         
         //base case
-        if(n == 0 || W == 0) return 0;
+        if(n == 0 || W == 0){
+            return 0;
+        }
         
         if(dp[n][W] != -1){
             return dp[n][W];
         }
         
-        // case for choice diagram
-        if(wt[n-1] <= W){
-            dp[n][W] =  max((val[n-1] + knap(val , wt , W-wt[n-1] , n-1)) , knap(val,wt,W,n-1));
-            return dp[n][W];
+        if(W >= wt[n-1]){
+            return dp[n][W] =  max(val[n-1] + knap(val,wt,W-wt[n-1],n-1) , knap(val,wt,W,n-1));
         }
-        
-        else if(wt[n-1] > W){
-            dp[n][W] =  knap(val,wt,W,n-1);
-            return dp[n][W];
-        }
+        else return dp[n][W] = knap(val,wt,W,n-1);
     }
+    
   
     int knapsack(int W, vector<int> &val, vector<int> &wt) {
         
-        memset(dp, -1, sizeof(dp));
-        
         int n = val.size();
         
-        return knap(val , wt , W , n);
+        memset(dp,-1,sizeof(dp));
+        
+        return knap(val,wt,W , n);
         
     }
 };
